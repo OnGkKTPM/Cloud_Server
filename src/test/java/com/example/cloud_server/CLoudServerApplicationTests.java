a4b7844235b8b6f52711f3e81795384102d9c2a6
YtalyPham <nhuyphamdev@gmail.com>
2021-10-27 21:51:48 +0700

    Cloud_Server

---
+++ src/test/java/com/example/cloud_server/CLoudServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cloud_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CLoudServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
